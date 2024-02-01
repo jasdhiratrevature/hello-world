4ca34f0d3e4df76b038fa93a630ad278512bb242
Jasdhir Singh <jasdhir.singh@revature.com>
2024-02-01 15:47:10 +0530

    abc

--- src/main/java/com/revature/gha/helloworld/HelloWorldApplication.java
+++ src/main/java/com/revature/gha/helloworld/HelloWorldApplication.java
@@ -1,13 +1,16 @@
 package com.revature.gha.helloworld;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloWorldApplication {
+public class HelloWorldApplication  {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
+
 
 }
